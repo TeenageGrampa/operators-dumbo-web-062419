1ee1878d0e6a45bef7889943146e7b6ebc157a6e
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-18 17:44:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,14 +2,14 @@ def unsafe?(speed)
   if 40 < speed < 60
     "safe"
   else 
-    "not safe"
+    "unsafe"
   end
 end
 
 
 
 def not_safe?(speed)
-40 < speed < 60 ? "safe" : "not safe"	
+40 < speed < 60 ? "safe" : "unsafe"	
 end
 	
 
